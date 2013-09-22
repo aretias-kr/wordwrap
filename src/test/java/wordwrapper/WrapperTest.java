6777ce8d5ea9fe5dc8f8f47c7b4cc66adbaea693
이정인 <aretias.kr@gmail.com>
2013-09-22 23:13:49 +0900

    stuck된 상태를 맞이한 WordWrapper

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -1,9 +1,19 @@
 package wordwrapper;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 public class WrapperTest {
-    @Test
-    public void nothing() {
-    }
+	@Test public void
+	should_wrap() {
+	    assertThat(wrap("word word", 4), is("word\nword"));
+	    assertThat(wrap("a dog", 5), is("a dog"));
+	    assertThat(wrap("a dog with a bone", 6), is("a dog\nwidth a\nbone"));
+	}
+	
+	private String wrap(String s, int width) {
+	    return s.length() > width ? s.replaceAll(" ", "\n") : s;
+	}
 }
